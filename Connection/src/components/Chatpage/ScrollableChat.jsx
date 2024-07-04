1092b9e5070d2c3270a3906b92a18664aa50ed49
shadil <shadilsa786@gmai.com>
2024-07-04 22:52:17 +0530

    resolved

--- Connection/src/components/Chatpage/ScrollableChat.jsx
+++ Connection/src/components/Chatpage/ScrollableChat.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import ScrollableFeed from "react-scrollable-feed";
 import { useChatState } from "../../context/ChatProvider";
 import { TimeMange } from "../Authentication/TimeManagment";
-import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import { DeleteMessage, EditingMessage } from "../../api/UserApi";
 import toast from "react-hot-toast";
 
@@ -12,6 +11,7 @@ function ScrollableChat({ messages, fetchMessages }) {
   const [deleteMessage, setDeleteMessage] = useState();
   const [editingMessage, setEditingMessage] = useState(null);
   const [newContent, setNewContent] = useState("");
+  const [contextMenu, setContextMenu] = useState(null);
 
   useEffect(() => {
     scroll.current?.scrollIntoView({ behavior: "smooth" });
@@ -22,6 +22,7 @@ function ScrollableChat({ messages, fetchMessages }) {
     toast(response.message);
     setDeleteMessage(messageId);
   };
+
   useEffect(() => {
     if (deleteMessage) {
       fetchMessages();
@@ -31,6 +32,7 @@ function ScrollableChat({ messages, fetchMessages }) {
   const handleEdit = (message) => {
     setEditingMessage(message);
     setNewContent(message.content);
+    setContextMenu(null);
   };
 
   const handleKeyPress = async (e, messageId) => {
@@ -41,80 +43,114 @@ function ScrollableChat({ messages, fetchMessages }) {
       });
       toast(response.message);
       fetchMessages();
+      setEditingMessage(null);
     }
   };
 
+  const handleRightClick = (event, message) => {
+    event.preventDefault();
+    if (message.sender.id === user.id) {
+      setContextMenu({
+        x: event.clientX,
+        y: event.clientY,
+        message,
+      });
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    if (contextMenu) {
+      setContextMenu(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [contextMenu]);
+
   return (
-    <div className="h-screen overflow-y-auto p-4 pb-36 hidescroll">
+    <div className="h-screen overflow-y-auto p-4 pb-36 hidescroll" onContextMenu={(e) => e.preventDefault()}>
       <ScrollableFeed>
         {messages &&
           messages.map((m, i) => (
-            <ContextMenuTrigger id={`contextmenu-${m.id}`} key={i}>
-              <div
-                className={`flex mb-4 cursor-pointer ${
-                  m.sender.id === user.id ? "justify-end" : "justify-start"
-                }`}
-              >
-                {m.sender.id !== user.id && (
-                  <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
-                    <img
-                      src={m.sender.picture}
-                      alt="User Avatar"
-                      className="w-8 h-8 rounded-full"
-                    />
-                  </div>
-                )}
-                <div
-                  className={`flex max-w-96 rounded-lg p-3 gap-3 ${
-                    m.sender.id === user.id
-                      ? "bg-[#8338ec] text-white"
-                      : "bg-gray-100 text-black"
-                  } ${m.file && "flex flex-col "}`}
-                >
-                  {m.file && <img src={m.file} alt="" className="w-36" />}
-                  {editingMessage?.id === m.id ? (
-                    <input
-                      type="text"
-                      value={newContent}
-                      onChange={(e) => setNewContent(e.target.value)}
-                      onKeyPress={(e) => handleKeyPress(e, m.id)}
-                      autoFocus
-                      className="bg-gray-400 rounded-lg p-2 text-black"
-                    />
-                  ) : (
-                    <p>{m.content}</p>
-                  )}
-                  <span className="flex justify-end items-end text-[10px] font-prompt-light pt-2">
-                    {TimeMange(m.createdAt) === "NaN years ago"
-                      ? "just now"
-                      : TimeMange(m.createdAt)}
-                  </span>
+            <div
+              className={`flex mb-4 cursor-pointer ${
+                m.sender.id === user.id ? "justify-end" : "justify-start"
+              }`}
+              key={i}
+              onContextMenu={(e) => handleRightClick(e, m)}
+            >
+              {m.sender.id !== user.id && (
+                <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
+                  <img
+                    src={m.sender.picture}
+                    alt="User Avatar"
+                    className="w-8 h-8 rounded-full"
+                  />
                 </div>
-              </div>
-              <ContextMenu id={`contextmenu-${m.id}`}>
-                {!m.deleted && (
-                  <div className="bg-gray-200 p-2 shadow-md">
-                    <MenuItem
-                      key={`edit-${m.id}`}
-                      onClick={() => handleEdit(m)}
-                      className="cursor-pointer"
-                    >
-                      Edit
-                    </MenuItem>
-                    <MenuItem
-                      key={`delete-${m.id}`}
-                      onClick={() => handleDelete(m.id)}
-                      className="cursor-pointer"
-                    >
-                      Delete
-                    </MenuItem>
-                  </div>
+              )}
+              <div
+                className={`flex max-w-96 rounded-lg p-3 gap-3 ${
+                  m.sender.id === user.id
+                    ? "bg-[#8338ec] text-white"
+                    : "bg-gray-100 text-black"
+                } ${m.file && "flex flex-col "}`}
+              >
+                {m.file && <img src={m.file} alt="" className="w-36" />}
+                {editingMessage?.id === m.id ? (
+                  <input
+                    type="text"
+                    value={newContent}
+                    onChange={(e) => setNewContent(e.target.value)}
+                    onKeyPress={(e) => handleKeyPress(e, m.id)}
+                    autoFocus
+                    className="bg-gray-400 rounded-lg p-2 text-black"
+                  />
+                ) : (
+                  <p>{m.content}</p>
                 )}
-              </ContextMenu>
-            </ContextMenuTrigger>
+                <span className="flex justify-end items-end text-[10px] font-prompt-light pt-2">
+                  {TimeMange(m.createdAt) === "NaN years ago"
+                    ? "just now"
+                    : TimeMange(m.createdAt)}
+                </span>
+              </div>
+            </div>
           ))}
         <div ref={scroll}></div>
       </ScrollableFeed>
+      {contextMenu && (
+        <div
+          style={{
+            position: "absolute",
+            top: `${contextMenu.y}px`,
+            left: `${contextMenu.x}px`,
+            background: "white",
+            border: "1px solid gray",
+            borderRadius: "4px",
+            boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            zIndex: 1000,
+            padding: "5px",
+          }}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button
+            onClick={() => handleEdit(contextMenu.message)}
+            className="block px-4 py-2 text-xs text-blue-500"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => handleDelete(contextMenu.message.id)}
+            className="block px-4 py-2 text-xs text-red-500"
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 }
